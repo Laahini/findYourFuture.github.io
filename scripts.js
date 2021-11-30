2c9fa91366275d001b76ab0c9ede670a1d1676e5
Laahini <62526180+Laahini@users.noreply.github.com>
2021-11-29 23:29:34 -0500

    Create folder and index.html
    Created base structure for website, did more research for inspiration, have some new ideas

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//Parallax scroll - Home
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0)
+})
