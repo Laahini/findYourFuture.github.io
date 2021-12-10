532e30c13cf9aaffc26201795dc44ed37d7060a6
Laahini <62526180+Laahini@users.noreply.github.com>
2021-12-10 07:20:14 -0500

    update schedule boxes

--- scripts.js
+++ scripts.js
@@ -35,3 +35,30 @@ function filterEvents(c)
     if(x[i].className.indexOf(c) >-1) addClass(x[i],?"show");
   }
 }
+function addClass(element, name)
+{
+  var i = arr1, arr2;
+  arr1= element.className.split(" ");
+  arr2=name.split(" ");
+  for (i = 0; i<arr2.length;i++)
+  {
+    if(arr1.indexOf(arr2[i]==-1)
+    {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+function removeClass(element, name)
+{
+  var i, arr1, arr2;
+  arr1= element.className.split(" ");
+  arr2=name.split(" ");
+  for (i=0; i<arr2.length; i++)
+  {
+    while(arr1.indexOf(arr2[i])>-1)
+    {
+      arr1.splice(arr1.indexOf(arr2[i],1))
+    }
+  }
+  element.className = arr1.join(" ");
+}
